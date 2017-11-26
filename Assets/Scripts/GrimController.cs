7d05cdf01a9c6afc336e3a8d85b2d23e98786a34
collinharmon <collin.harmon1@gmail.com>
2017-11-26 00:32:07 -0800

    added another section with little rocks as platforms. upon reaching this part of the game the grim reaper speeds up in his chase.

--- Assets/Scripts/GrimController.cs
+++ Assets/Scripts/GrimController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class GrimController : MonoBehaviour {
 	float speed = 5f;
 	bool occured = false;
+	bool occured2 = false;
 	// Use this for initialization
 	void Start () {
-		
+		speed = 5f;
 	}
 	
 	// Update is called once per frame
@@ -16,9 +17,16 @@ public class GrimController : MonoBehaviour {
 			speed+=.0222f;
 			//occured = true;
 		}*/
+		if(TimeScore.scoreCount-PlayerController.starGrab < 30){
+			speed = 5f;
+		}
 		if(TimeScore.scoreCount-PlayerController.starGrab > 30 && occured == false){
 			speed+=1.8f;
 			occured = true;
+		}
+		if(TimeScore.scoreCount-PlayerController.starGrab > 39 && occured2 == false){
+			speed+=2.2f;
+			occured2 = true;
 		}
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
