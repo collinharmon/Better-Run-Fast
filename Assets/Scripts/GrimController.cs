c67f9fe0650e7b74737786de61590d3c1cb4f0a1
collinharmon <collin.harmon1@gmail.com>
2017-11-25 23:05:32 -0800

    Extended the game/added platforms. Also added more fireballs and more stars.

--- Assets/Scripts/GrimController.cs
+++ Assets/Scripts/GrimController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrimController : MonoBehaviour {
-
+	float speed = 5f;
+	bool occured = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +12,15 @@ public class GrimController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (5, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+		/*if(TimeScore.scoreCount-PlayerController.starGrab > 30 && occured == false){
+			speed+=.0222f;
+			//occured = true;
+		}*/
+		if(TimeScore.scoreCount-PlayerController.starGrab > 30 && occured == false){
+			speed+=1.8f;
+			occured = true;
+		}
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.tag == "Player"){
